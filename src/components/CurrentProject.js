f522e947c08f6f11d4ae1b044535ddb0b25f2aa4
Andrew <atklenner@gmail.com>
2022-03-22 21:36:26 -0700

    Finish task component

--- src/components/CurrentProject.js
+++ src/components/CurrentProject.js
@@ -2,25 +2,31 @@ import useTasks from "../hooks/useTasks";
 import { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 import { useProjectContext } from "../context/ProjectContext";
+import Task from "./Task";
 
 export default function CurrentProject() {
   const { selectedProject, projects } = useProjectContext();
   const [tasks] = useTasks();
   const [showAddTaskForm, setShowAddTaskForm] = useState(false);
+
+  // function deleteCompletedTasks() {
+
+  // }
+
   return (
     <div>
-      <h2>{projects.find((project) => project.id === selectedProject).name}</h2>
-      {/* the ul will need to change completely */}
-      <ul role="list">
+      <h2>
         {selectedProject &&
-          tasks
-            .filter((task) => task.projectId === selectedProject)
-            .map((task) => <li key={task.id}>{task.task}</li>)}
-      </ul>
+          projects.find((project) => project.id === selectedProject).name}
+      </h2>
+      {selectedProject &&
+        tasks
+          .filter((task) => task.projectId === selectedProject)
+          .map((task) => <Task key={task.id} {...task} />)}
       {selectedProject && !showAddTaskForm && (
         <div>
           <button onClick={() => setShowAddTaskForm(true)}>Add Task</button>
-          <button>Remove Completed Tasks</button>
+          {/* <button onClick={deleteCompletedTasks}>Remove Completed Tasks</button> */}
         </div>
       )}
       {showAddTaskForm && (
---
+++ src/components/Task.js
@@ -0,0 +1,27 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import styles from "../styles/Task.module.scss";
+import { GrTrash, GrCheckbox, GrCheckboxSelected } from "react-icons/gr";
+
+export default function Task({ id, task, archived, projectId }) {
+  function archiveTask() {
+    updateDoc(doc(db, "tasks", id), {
+      archived: !archived,
+    });
+  }
+
+  function deleteTask() {
+    deleteDoc(doc(db, "tasks", id));
+  }
+  return (
+    <div className={styles.container}>
+      <div onClick={archiveTask}>
+        {archived ? <GrCheckboxSelected /> : <GrCheckbox />}
+      </div>
+      <p className={archived ? styles.archived : ""}>{task}</p>
+      <div onClick={deleteTask}>
+        <GrTrash />
+      </div>
+    </div>
+  );
+}
